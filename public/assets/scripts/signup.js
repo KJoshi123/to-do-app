479cb7d508f5f2c734f61188a2bc600aef2e403f
kaustubh joshi <joshigikaustubh@gmail.com>
2021-01-05 08:34:40 +0530

    signup is working, but with redirection issue

--- public/assets/scripts/signup.js
+++ public/assets/scripts/signup.js
@@ -1,18 +1,33 @@
 $(document).ready(function(){
 
-    $('form').on('submit', ()=>{
+    $('form').on('submit', async ()=>{
         const email = $("#name").val();
         const password = $("#password").val();
         console.log("username : ", username, " password: ",password);
 
-        $.ajax({
-            url: "/signup",
-            method: "POST",
-            data: { email, password}
-        }).done( function(response){
-            console.log(response);
-        }).fail( function(error) {
-            console.log(error);
-        });
-    })
+        /*$.ajax({
+            type: 'POST',
+            url: '/signup',
+            contentType: 'application/json',
+            async: true,
+            data: { email, password},
+            success: function(data){
+                console.log(data);
+            },
+            fail: function(data){
+                console.log(data);
+            }
+        });*/
+
+        try{
+            const res = await fetch('/signup', {
+                method:'POST',
+                body:{ email: email, password: password}
+            });
+            console.log(res);
+        }
+        catch(error){
+            console.log(error)
+        }
+    });
 });
